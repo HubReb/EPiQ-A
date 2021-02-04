6db98ba82b289ec4e1f132b86989e0fdb3cc32be
Rebekka Hubert <hubert@cl.uni-heidelberg.de>
2021-02-04 21:39:03 +0100

    minor bug fixes

--- src/data_generation/merge_article_versions.py
+++ src/data_generation/merge_article_versions.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+""" Merging of articles to have all article version in one article without duplicated paragraphs """
+
 import re
 import sys
 import csv
@@ -11,7 +15,7 @@ def merge_article_versions(filename: str) -> None:
     old_max_filed_size = csv.field_size_limit()
     csv.field_size_limit(sys.maxsize)
     articles = dict()
-    
+
     # First, we read all articles and bucket them by title
     # The title is always in the first line of the article text
     with open(filename) as wf:
@@ -20,44 +24,44 @@ def merge_article_versions(filename: str) -> None:
                 continue
             title = text.split('\n')[0].strip().lower()
             title = re.sub('- wikipedia', '', title).strip()
-            
+
             texts = text.split('\n\n')
-            
+
             if title in articles:
                 articles[title]['links'].append(link)
                 articles[title]['texts'].extend(texts)
-            
+
             else:
                 articles[title] = {'links': [link], 'texts': texts}
-    
+
     # Next, we create a new file containing all merged articles
     # We remove duplicate paragraphs and save wikipedia links
     # of all articles that got merged
-    name, extension = filename.split('.')
-    with open(name + '_merged' + '.' + extension, 'w') as wf:
+    name, _ = filename.split('.csv')
+    with open(name + '_merged' + '.csv', 'w') as wf:
         # Write header
         csv_writer = csv.writer(wf, delimiter=',')
-        csv_writer.writerow(['',"Wikipedia_ID", "Text"])
+        csv_writer.writerow(['', "Wikipedia_ID", "Text"])
 
         deleted_paragraph_counter = 0
 
         for index, (title, article_info) in enumerate(articles.items()):
-            print("Processing article {}/{}\tNum deleted paragraphs: {}" \
-                .format(index+1, len(articles), deleted_paragraph_counter), end='\r')
-            links = article_info['links']¸
+            print("Processing article {}/{}\tNum deleted paragraphs: {}"
+                  .format(index+1, len(articles), deleted_paragraph_counter), end='\r')
+            links = article_info['links']
             texts = article_info['texts']
-            
+
             keep_paragraphs = []
             for paragraph in texts:
                 # Within a paragraph, we can ignore all other types
                 # of whitespace
-                paragraph = re.sub("\s", " ", paragraph)
+                paragraph = re.sub(r"\s", " ", paragraph)
                 paragraph = paragraph.lower().strip()
-                
+
                 # Remove empty paragraphs
                 if not paragraph:
                     continue
-                
+
                 # Compare current paragraph to all previous paragraphs
                 # of the same merged article. Only keep unseen paragraphs.
                 keep = True
@@ -66,13 +70,13 @@ def merge_article_versions(filename: str) -> None:
                         keep = False
                         deleted_paragraph_counter += 1
                         break
-                
+
                 if keep:
                     keep_paragraphs.append(paragraph)
-            
+
             if len(keep_paragraphs) >= 1:
                 text = "\n\n".join(keep_paragraphs)
-                
+                print(text)
                 csv_writer.writerow([str(index), " ".join(links), text])
 
     csv.field_size_limit(old_max_filed_size)
@@ -80,6 +84,3 @@ def merge_article_versions(filename: str) -> None:
 
 if __name__ == '__main__':
     merge_article_versions("../../data//article_retrieval/nq_dev_train_wiki_text.csv")
-    
-    
-    
