b3b4f06f161f9e073f499702baccb2469dc0d1ae
Rebekka Hubert <hubert@cl.uni-heidelberg.de>
2021-02-23 22:41:53 +0100

    add informative cli output

--- src/article_retrieval/train.sh
+++ src/article_retrieval/train.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+echo "Merging natural questions training and dev set articles,,,"
 python fuse.py
+echo "Creating inverted index... This step may take several hours (depending on your hardware)"
 python inverted_index.py
 cd ../data_generation/
+echo "Creating the merged dataset by fusing different versions of one article into a single article..."
 python merge_article_versions.py
 cd -
+echo "Training the models... This may take several minutes..."
 python train_models.py
